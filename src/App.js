e66f981d5a49a8d72c4e0fc9791d90cdcdbf9838
Guilherme Braga <guilherme_braga100@hotmail.com>
2023-06-05 20:28:33 +0000

    Padrinização de código

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const router = require('./Router');
+const express = require('express')
+const cors = require('cors')
+const router = require('./Router')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 app.use((req, res, next) => {
-    console.log("access");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
-    app.use(cors());
-    next();
-});
-app.use(router);
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with")
+    app.use(cors())
+    next()
+})
 
-module.exports = app;
+app.use(router)
+
+module.exports = app
