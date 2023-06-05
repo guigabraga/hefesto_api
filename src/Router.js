65be45c014d3aca89db083a0806c468f45fba421
Guilherme Braga <guilherme_braga100@hotmail.com>
2023-06-05 20:29:01 +0000

    Padronização de código

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 const ControllerTestDb = require('./controllers/controllerTestDb')
-const ControllerCronnosDb = require('./controllers/CotrollerCronnosDb');
+const ControllerCronnosDb = require('./controllers/CotrollerCronnosDb')
 
 router.get('/testDb', ControllerTestDb.ControllerTestDb)
 router.post('/new-user', ControllerCronnosDb.CreateUser)
 router.post('/select-user', ControllerCronnosDb.SelectUser)
 
-module.exports = router;
+module.exports = router
